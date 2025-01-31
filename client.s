e931e22cdeac85c75c71eed83d002b37bf70a584
ryan <ryan.rais@ynov.com>
2025-01-31 21:09:58 +0100

    ui

--- client.s
+++ client.s
@@ -1,58 +1,49 @@
-section .data
-  request db "Hello Server!",0xa
-  len_request equ $-request
-  response db ''
 section .bss
-  socket resb 8
-  buf resb 32
-  port resb 16
-  server resb 16
-  saved resb 8
-section .text
-  global _start
-    _start:
-        call main
-	call exit
-    main:
-        push rbp
-        mov rbp,rsp
-        mov rdx, 0
-        mov rsi, 1
-        mov rdi, 2
-        mov rax, 41
-        syscall ; sys_socket
-        mov [socket], rax
-        mov word [server], 0x2
-        mov ebx, 0xfeffff80 ; inverse of 127.0.0.1
-        not ebx ; noted to become 127.0.0.1
-        mov dword [server+4], ebx
-        mov word [port], 0x5c11 ; 4444
-        movzx rax, word[port]
-        mov word [server+2], ax
-        mov rax, [socket]
-        mov rdx, 0x10
-        lea rsi, [server]
-        mov rdi, rax
-        mov rax, 42
-        syscall ; sys_connect
-        mov rax, 1
-        mov rdi, [socket] ; write destination = socket
-        mov rdx, len_request
-        mov rsi, request
-        syscall ; sys_write
-        mov rax, 0
-        mov rdi, [socket] ; read destination = socket
-        mov rdx, 14
-        mov rsi, response
-        syscall ; sys_read
-        mov rax, 1
-        mov rdi, 1 ; write destination != socket
-        syscall ; sys_write
-        mov rax, 0
-        pop rbp
-	ret
-    exit:
-	mov    rax, 60
-	mov    rdi, 0
-	syscall
+    key_buffer resb 1
+    socket resq 1
+    server resb 16
 
+section .data
+    ip_address db "127.0.0.1", 0
+    port dw 4444
+
+section .text
+    global _start
+
+_start:
+    ; 1. Créer un socket
+    mov rax, 41      ; syscall socket
+    mov rdi, 2       ; AF_INET
+    mov rsi, 1       ; SOCK_STREAM
+    mov rdx, 0       ; IPPROTO_IP
+    syscall
+    mov [socket], rax
+
+    ; 2. Configurer l'adresse du serveur
+    mov word [server], 2     ; AF_INET
+    mov word [server+2], 0x5c11 ; Port 4444
+    mov dword [server+4], 0x0100007F ; 127.0.0.1 en hex
+
+    ; 3. Connecter au serveur
+    mov rax, 42
+    mov rdi, [socket]
+    lea rsi, [server]
+    mov rdx, 16
+    syscall
+
+read_keys:
+    ; 4. Lire une touche
+    mov rax, 0    ; syscall read
+    mov rdi, 0    ; stdin
+    mov rsi, key_buffer
+    mov rdx, 1
+    syscall
+
+    ; 5. Envoyer la touche au serveur
+    mov rax, 1    ; syscall write
+    mov rdi, [socket]  ; socket du serveur
+    mov rsi, key_buffer
+    mov rdx, 1
+    syscall
+
+    jmp read_keys  ; Boucle infinie pour capturer les frappes
--- server.s
+++ server.s
@@ -1,66 +1,66 @@
-section .data
-  response db 'Hello Client!',0xa
-  len_response equ $-response
-  buf db ''
 section .bss
-  socket resb 8
-  port resb 16
-  server resb 16
-  client resb 16
-section .text
-  global _start
-    _start:
-        call main
-        call exit
-    main:
-        push rbp
-        mov rbp,rsp
-        mov rdx, 0
-        mov rsi, 1
-        mov rdi, 2
-        mov rax, 41
-        syscall ; sys_socket
-        mov [socket], rax
-        mov word [server], 0x2
-        mov ebx, 0xfeffff80 ; noted 127.0.0.1
-        not ebx
-        mov dword [server+4], ebx
-        mov word [port], 0x5c11 ; 4444
-        movzx rax, word[port]
-        mov word [server+2], ax
-        mov rax, [socket]
-        mov rdx, 0x10
-        lea rsi, [server] ; load server structure
-        mov rdi, rax
-        mov [client], rdi
-        push 49
-        pop rax
-        syscall ; sys_bind
-        pop rsi
-        push 50
-        pop rax
-        syscall ; sys_listen
-        mov rax, 43
-        syscall ; sys_accept
-        mov rdi, rax
-        mov r8d, eax
-        mov rax, 0
-        mov rdx, 14
-        mov rsi, buf
-        syscall ; sys_read
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, buf
-        mov rdx, 14
-        syscall ; sys_write (local)
-        mov rsi, 0
-        mov rax, 1
-        mov rdi, r8
-        mov rsi, response
-        mov rdx, len_response
-        syscall ; sys_write (remote)
-    exit:
-        mov    rax, 60
-        mov    rdi, 0
-        syscall
+    socket resq 1
+    client_socket resq 1
+    server resb 16
+    key_buffer resb 1
 
+section .data
+    port dw 4444
+
+section .text
+    global _start
+
+_start:
+    ; 1. Créer un socket
+    mov rax, 41  ; syscall socket
+    mov rdi, 2   ; AF_INET
+    mov rsi, 1   ; SOCK_STREAM
+    mov rdx, 0   ; IPPROTO_IP
+    syscall
+    mov [socket], rax
+
+    ; 2. Bind l'adresse du serveur
+    mov word [server], 2     ; AF_INET
+    mov word [server+2], 0x5c11 ; Port 4444
+    mov dword [server+4], 0x00000000 ; 0.0.0.0 (écoute toutes les IPs)
+
+    mov rax, 49  ; syscall bind
+    mov rdi, [socket]
+    lea rsi, [server]
+    mov rdx, 16
+    syscall
+
+    ; 3. Écoute sur le port
+    mov rax, 50  ; syscall listen
+    mov rdi, [socket]
+    mov rsi, 10  ; max clients en attente
+    syscall
+
+accept_loop:
+    ; 4. Accepter une connexion
+    mov rax, 43  ; syscall accept
+    mov rdi, [socket]
+    mov rsi, 0
+    mov rdx, 0
+    syscall
+    mov [client_socket], rax  ; Stocke le socket client
+
+read_loop:
+    ; 5. Lire une touche
+    mov rax, 0
+    mov rdi, [client_socket]
+    mov rsi, key_buffer
+    mov rdx, 1
+    syscall
+
+    cmp rax, 0  ; Si la connexion est fermée, accepter un nouveau client
+    jle accept_loop
+
+    ; 6. Afficher la touche reçue
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, key_buffer
+    mov rdx, 1
+    syscall
+
+    jmp read_loop  ; Reprendre la lecture
